cafbbbe719498133282abef3d4b2da9946d06608
Theodore Wittmann <theo@horace.co>
2025-04-02 23:39:25 +0200

    Suppression de composer du repo

--- test/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/../vendor/autoload.php');
-if (PHP_MAJOR_VERSION >= 7 && PHP_MINOR_VERSION > 1)
-    require_once(__DIR__ . '/Unidays/TestCaseBase.php');
-else
-    require_once(__DIR__ . '/Unidays/TestCaseBase5.php');
